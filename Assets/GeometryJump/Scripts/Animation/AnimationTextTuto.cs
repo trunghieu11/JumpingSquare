61f1b8f2e3f25ae23b896793edb130fc309e9700
trunghieu11 <vuondenthanhcong11@gmail.com>
2017-07-08 19:52:18 +0700

    Initial.

---
+++ Assets/GeometryJump/Scripts/Animation/AnimationTextTuto.cs
@@ -0,0 +1,140 @@
+﻿/***********************************************************************************************************
+ * Produced by App Advisory - http://app-advisory.com													   *
+ * Facebook: https://facebook.com/appadvisory															   *
+ * Contact us: https://appadvisory.zendesk.com/hc/en-us/requests/new									   *
+ * App Advisory Unity Asset Store catalog: http://u3d.as/9cs											   *
+ * Developed by Gilbert Anthony Barouch - https://www.linkedin.com/in/ganbarouch                           *
+ ***********************************************************************************************************/
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+#if AADOTWEEN
+using DG.Tweening;
+#endif
+
+/// <summary>
+/// Class in charge to animate the text "short jump" and "long jump" at start
+/// </summary>
+namespace AppAdvisory.GeometryJump
+{
+	public class AnimationTextTuto : MonoBehaviorHelper 
+	{
+		public float delay = 0;
+
+		bool firstTouchDone = false;
+
+		public RectTransform textLeft;
+		public RectTransform textRight;
+
+		float animTime
+		{
+			get
+			{
+				return playerManager.animTime/2f;
+			}
+		}
+
+		void OnEnable()
+		{
+			InputTouch.OnTouchLeft += OnTouchLeft;
+			InputTouch.OnTouchRight += OnTouchRight;
+			CanvasManager.OnAnimationTransitionOutStart += DoAnimIn;
+		}
+
+		void OnDisable()
+		{
+			InputTouch.OnTouchLeft -= OnTouchLeft;
+			InputTouch.OnTouchRight -= OnTouchRight;
+			CanvasManager.OnAnimationTransitionOutStart -= DoAnimIn;
+		}
+
+		void OnTouchLeft()
+		{
+			#if AADOTWEEN
+			textLeft.DOScale(Vector2.one * 0.8f,animTime)
+				.SetEase(Ease.OutBack,0.6f,1)
+				.OnComplete( () => {
+					textLeft.DOScale(Vector2.one,animTime)
+						.SetEase(Ease.InBack,0.6f,1);
+				});
+			#endif
+			OnTouch();
+		}
+
+		void OnTouchRight()
+		{
+			#if AADOTWEEN
+			textRight.DOScale(Vector2.one * 0.8f,animTime)
+				.SetEase(Ease.OutBack,0.6f,1)
+				.OnComplete( () => {
+					textRight.DOScale(Vector2.one,animTime)
+						.SetEase(Ease.InBack,0.6f,1);
+				});
+			#endif
+			OnTouch();
+		}
+
+		void OnTouch()
+		{
+			if(!firstTouchDone)
+			{
+				firstTouchDone = true;
+				Invoke("DoAnimOut",2);
+			}
+		}
+
+		public void DoAnimIn()
+		{
+			CanvasManager.OnAnimationTransitionOutStart -= DoAnimIn;
+
+			int mult = 1;
+
+			foreach(Transform t in transform)
+			{
+				var pos = t.GetComponent<RectTransform>().anchoredPosition;
+
+				float xOrigin = pos.x;
+
+				pos.x = mult *  2 * Screen.width;
+				mult *= -1;
+				t.GetComponent<RectTransform>().anchoredPosition = pos;
+				#if AADOTWEEN
+				t.GetComponent<RectTransform>().DOLocalMoveX(xOrigin, 1)
+					.SetDelay(delay)
+					.SetEase(Ease.OutBack,0.6f,1);
+				#endif
+			}
+		}
+
+		public void DoAnimOut()
+		{
+			int mult = 1;
+
+			foreach(Transform t in transform)
+			{
+				var pos = t.GetComponent<RectTransform>().anchoredPosition;
+
+				float xOrigin = pos.x;
+
+				#if AADOTWEEN
+				t.GetComponent<RectTransform>().DOLocalMoveX(mult *  2 * Screen.width, 1)
+					.SetDelay(delay)
+					.SetEase(Ease.InBack,0.6f,1)
+					.OnComplete( () => {
+						gameObject.SetActive(false);
+						var p = t.GetComponent<RectTransform>().anchoredPosition;
+						p.x = xOrigin;
+						t.GetComponent<RectTransform>().anchoredPosition = pos;
+
+						gameObject.SetActive(false);
+					});
+				#endif
+
+				mult *= -1;
+			}
+		}
+	}
+}
---
+++ Assets/GeometryJump/Scripts/ColorManager.cs
@@ -0,0 +1,88 @@
+﻿/***********************************************************************************************************
+ * Produced by App Advisory - http://app-advisory.com													   *
+ * Facebook: https://facebook.com/appadvisory															   *
+ * Contact us: https://appadvisory.zendesk.com/hc/en-us/requests/new									   *
+ * App Advisory Unity Asset Store catalog: http://u3d.as/9cs											   *
+ * Developed by Gilbert Anthony Barouch - https://www.linkedin.com/in/ganbarouch                           *
+ ***********************************************************************************************************/
+
+using UnityEngine;
+using System.Collections;
+#if AADOTWEEN
+using DG.Tweening;
+#endif
+
+/// <summary>
+/// Class in charge to manage the background colors
+/// </summary>
+namespace AppAdvisory.GeometryJump
+{
+	public class ColorManager : MonoBehaviorHelper 
+	{
+		public Color[] colors;
+
+		public float timeChangeColor = 10;
+
+		bool isGameOver = false;
+
+		void Awake()
+		{
+			Color c = PlayerPrefsX.GetColor("BACKGROUND_COLOR", colors[0]);
+			cam.backgroundColor = c;
+		}
+
+		void OnEnable()
+		{
+			GameManager.OnGameStart += OnGameStart;
+
+			GameManager.OnGameOverStarted += OnGameOverStarted;
+
+			Color c = PlayerPrefsX.GetColor("BACKGROUND_COLOR", colors[0]);
+			cam.backgroundColor = c;
+		}
+
+		void OnDisable()
+		{
+			GameManager.OnGameStart -= OnGameStart;
+
+			GameManager.OnGameOverStarted -= OnGameOverStarted;
+
+		}
+
+		void OnGameStart()
+		{
+			isGameOver = false;
+			ChangeColor();
+		}
+
+		void OnGameOverStarted()
+		{
+			isGameOver = true;
+		}
+
+		void ChangeColor(){
+
+
+			Color colorTemp = colors [UnityEngine.Random.Range (0, colors.Length)];
+
+			#if AADOTWEEN
+			cam.DOColor(colorTemp,3f).SetEase(Ease.Linear)
+				.SetDelay(timeChangeColor)
+				.OnComplete(() => {
+					PlayerPrefsX.SetColor("BACKGROUND_COLOR", cam.backgroundColor);
+
+					if(!isGameOver)
+					{
+						ChangeColor();
+					}
+				});
+			#endif
+
+		}
+
+		void OnApplicationQuit()
+		{
+			PlayerPrefs.Save();
+		}
+	}
+}
